7bc6c08797facef2b9de013be78f8d470237083a
salohy-onnelin25 <onnelinsaloh@gmail.com>
2025-10-28 21:37:14 +0300

    28/10/25 à 21h37

--- Assets/index.js
+++ Assets/index.js
@@ -8,12 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
   if (menuToggle && hamburgerIcon) {
     function toggleHamburgerIcon() {
       if (menuToggle.checked) {
-        // Menu ouvert -> Afficher l'icône X
         hamburgerIcon.classList.remove("fa-bars");
-        // Note: fa-times est obsolète. Utilisez fa-xmark (Font Awesome 6)
         hamburgerIcon.classList.add("fa-xmark");
       } else {
-        // Menu fermé -> Afficher l'icône hamburger
         hamburgerIcon.classList.remove("fa-xmark");
         hamburgerIcon.classList.add("fa-bars");
       }
@@ -24,17 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   function handleScroll() {
         if (window.scrollY > scrollThreshold) {
-            // Défilement > 50px : Appliquer le style "scrollé" (Fond noir, Barres blanches)
+            
             headerTop.classList.add("scrolled");
         } else {
-            // Défilement < 50px : Revenir au style initial (Fond blanc, Barres noires)
+           
             headerTop.classList.remove("scrolled");
         }
     }
-    
-    // Attacher l'écouteur d'événement au défilement de la fenêtre
     window.addEventListener("scroll", handleScroll);
-    
-    // Exécuter la fonction une fois au chargement pour gérer le cas où la page charge déjà scrollée
     handleScroll();
 });
